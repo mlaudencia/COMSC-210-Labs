b4df38629cc45fb66de21885eeaa8e5b108b3a9a
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-03 20:50:52 -0700

    Organizing file

--- Lab-3/lab3.cpp
+++ Lab-3/lab3.cpp
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 
-struct Restaurant{  // creates a new Restaurant
+struct Restaurant {  // creates a new Restaurant
     string name;
     string address;
     string cuisine;
@@ -12,41 +12,55 @@ struct Restaurant{  // creates a new Restaurant
     int capacity;
 };
 
-Restaurant newRestaurant(){  // newRestaurant() takes in user inputs for the name, address, type of cuisine, rating, and capacity of a
-    Restaurant temp;         // restaurant, and returns the new restaurant.
-    cout << "Enter Restaurant name: " << endl;
-    getline(cin, temp.name); // used getline() here, since when I used cin for all user inputs, it skipped inputs, and getline() doesn't
-    cout << "Enter Restaurant address: " << endl;
-    getline(cin, temp.address);
-    cout << "Enter Restaurant cuisine: " << endl;
-    getline(cin, temp.cuisine);
-    cout << "Enter Restaurant rating: " << endl;
-    cin >> temp.rating;      // I didn't use getline() here, because that skipping input error was happening just in the opposite way.
-    cout << "Enter Restaurant capacity: " << endl;
-    cin >> temp.capacity; 
-    cin.ignore(); // I was really confused about why some inputs were being skipped, and searching it up, I learned cin >>
-                  // leaves its newline when you press Enter and only reads the user input, so when the next restaurant comes around,
-                  // the newline left over only gets seen then and skips that getline, so nothing can be inputted for the next
-                  // restaurant's name.
-    return temp;
-}
+// newRestaurant() creates a new restaurant with user inputs. 
+// arguments: none
+// returns: A restaurant object that contains the user's inputted data.
+Restaurant newRestaurant();
 
-void printRestaurant(Restaurant r){ // printRestaurant() takes a restaurant passed in and prints its data one by one
-    cout << "Restaurant name: " << r.name << endl; 
-    cout << "   Address: " << r.address << endl;
-    cout << "   Cuisine: " << r.cuisine << endl;
-    cout << "   Rating: " << r.rating << endl;
-    cout << "   Capacity: " << r.capacity << endl;
-}
+// printRestaurant() takes a restaurant passed in and prints its data one by one
+// arguments: a Restaurant, r
+// returns: nothing
+void printRestaurant(Restaurant r);
+
 
 int main() {
 
-    Restaurant r1 = newRestaurant(); // Here, for the milestone 2, it says create four restaurant objects, then manipulate them?
-    Restaurant r2 = newRestaurant(); // I assume that meant print, so I just made four restaurants, and then printed them all
-    Restaurant r3 = newRestaurant(); // at the same time.
+    Restaurant r1 = newRestaurant(); // milestone 2 says create four restaurant objects, then
+    Restaurant r2 = newRestaurant(); // manipulate them? I assumed it meant print, so I made 
+    Restaurant r3 = newRestaurant(); // four restaurants and printed them all at the same time.
     Restaurant r4 = newRestaurant();
     printRestaurant(r1);             // Each one of these prints out the data for the restaurant inputted. 
     printRestaurant(r2);
     printRestaurant(r3);
     printRestaurant(r4);
+}
+
+Restaurant newRestaurant(){  
+    Restaurant temp;
+    cout << "Enter Restaurant name: " << endl;
+    getline(cin, temp.name); // used getline() here, since when I used cin for all user
+                             // inputs, it skipped inputs, and getline() doesn't
+    cout << "Enter Restaurant address: " << endl;
+    getline(cin, temp.address);
+    cout << "Enter Restaurant cuisine: " << endl;
+    getline(cin, temp.cuisine);
+    cout << "Enter Restaurant rating: " << endl;
+    cin >> temp.rating;      // I didn't use getline() here, because that 
+                             // skipping input error was happening again.
+    cout << "Enter Restaurant capacity: " << endl;
+    cin >> temp.capacity; 
+    cin.ignore(); // I was  confused why some inputs were being skipped in my old code. 
+                  // Searching it up, I learned cin >> leaves its newline when you press
+                  // Enter and only reads the user input, so when the next restaurant 
+                  // comes around, the newline left over only gets seen then and skips 
+                  // that getline, so nothing can be inputted for the nextrestaurant's name.
+    return temp;
+}
+
+void printRestaurant(Restaurant r){
+    cout << "Restaurant name: " << r.name << endl; 
+    cout << "   Address: " << r.address << endl;
+    cout << "   Cuisine: " << r.cuisine << endl;
+    cout << "   Rating: " << r.rating << endl;
+    cout << "   Capacity: " << r.capacity << endl;
 }
