df20caf9fa114b2eb7473a89bb5be5080470266d
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-03 19:53:48 -0700

    ~10 mins update

--- Lab-3/lab3.cpp
+++ Lab-3/lab3.cpp
@@ -24,10 +24,25 @@ Restaurant newRestaurant(){  // newRestaurant() takes in user inputs for the nam
     cin >> temp.rating;      // I didn't use getline() here, because that skipping input error was happening just in the opposite way.
     cout << "Enter Restaurant capacity: " << endl; // It would skip because of numbers, while the previous one skipped because of
     cin >> temp.capacity;                          // spaces in the input.
-
     return temp;
 }
 
+void printRestaurant(Restaurant r){ // printRestaurant() takes a restaurant passed in and prints its data one by one
+    cout << "Restaurant name: " << r.name << endl; 
+    cout << "   Address: " << r.address << endl;
+    cout << "   Cuisine: " << r.cuisine << endl;
+    cout << "   Rating: " << r.rating << endl;
+    cout << "   Capacity: " << r.capacity << endl;
+}
+
 int main() {
-    newRestaurant(); 
+
+    Restaurant r1 = newRestaurant(); // Here, for the milestone 2, it says create four restaurant objects, then manipulate them?
+    Restaurant r2 = newRestaurant(); // I assume that meant print, so I just made four restaurants, and then printed them all
+    Restaurant r3 = newRestaurant(); // at the same time.
+    Restaurant r4 = newRestaurant();
+    printRestaurant(r1);
+    printRestaurant(r2);
+    printRestaurant(r3);
+    printRestaurant(r4);
 }
