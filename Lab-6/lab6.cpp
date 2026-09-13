24ee9093ea347b83a6cff6e74e66264d10ca720a
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-12 18:50:57 -0700

    10+ minutes update, fixes, and commenting

--- Lab-6/lab6.cpp
+++ Lab-6/lab6.cpp
@@ -11,13 +11,29 @@ double* numbers = new double[SIZE];
 
 int main(){
     enterArrayData(numbers);
+    outputArrayData(numbers);
+    sumArray(numbers);
 }
 
 void enterArrayData(double* numbers){
     cout << "Data entry for the array:" << endl;
     for(int i = 0; i < SIZE; i++){
-        cout << "Element #" << i << ": ";
+        cout << "   > Element #" << i << ": ";
         cin >> numbers[i];
     }
     cout << "Data entry complete." << endl;
+}
+
+void outputArrayData(double* numbers){
+    cout << "Outputting array elements: ";
+    for(int i = 0; i < SIZE; i++)
+        cout << numbers[i] << " ";
+    cout << endl;
+}
+
+void sumArray(double* numbers){
+    double total;
+    for(int i = 0; i < SIZE; i++)
+        total += numbers[i];
+    cout << "Sum of values: " << total;
 }
