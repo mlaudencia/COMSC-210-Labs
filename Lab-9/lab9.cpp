1afc7feb0bb7a98722f788f7bbaa02c8d8c97fed
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-18 18:52:01 -0700

    10 mins updatE

--- Lab-9/lab9.cpp
+++ Lab-9/lab9.cpp
@@ -9,5 +9,14 @@ const int ARRAY_SIZE = 30;
 
 int main(){
     ifstream inFile("fighters.txt");
-    array<string, ARRAY_SIZE> fighterArray;
+    array<string, ARRAY_SIZE> fighters;
+
+    for(int i = 0; i < fighters.size(); ++i)
+        getline(inFile, fighters[i]);
+
+    inFile.close();
+
+    for(int i = 0; i < fighters.size(); ++i){
+        cout << i + 1 << " " << fighters[i] << endl;
+    }
 }
