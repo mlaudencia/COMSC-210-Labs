cdff8cb888a0222462c40ed6a579df4b0c86e9e6
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-18 19:30:53 -0700

    10+ minutes update (was in Q&A and forgot)

--- Lab-9/lab9.cpp
+++ Lab-9/lab9.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 const int ARRAY_SIZE = 30;
@@ -28,4 +29,26 @@ int main(){
     cout << "Front: " << fighters.front() << endl;
     cout << "Back: " << fighters.back() << endl;
     cout << "Is empty?: " << (fighters.empty() == 0? "Not Empty" : "Empty") << endl;
+    cout << "Array address: " << fighters.data() << endl;
+    cout << "Alphabetically sorted: ";
+    sort(fighters.begin(), fighters.end());
+    for(string name : fighters){
+        cout << name << endl;
+    }
+    cout << endl;
+    cout << "Reverse alphpabetically sorted: ";
+    sort(fighters.rbegin(), fighters.rend());
+    for(string name : fighters){
+        cout << name << endl;
+    }
+    cout << endl;
+    string targetName = "Kai Asakura";
+    array<string, ARRAY_SIZE>::iterator foundName;
+    foundName = find(fighters.begin(), fighters.end(), targetName);
+    cout << "Found Name: " << targetName;
+    if(foundName != fighters.end())
+        cout << " found in position " << foundName - fighters.begin() << endl;
+    else
+        cout << " was not found" << endl;
+    cout << "    Name: " << *foundName << endl;
 }
