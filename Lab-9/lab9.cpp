825c6e5d6b57a595672555be0224ff75114e82f1
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-18 18:43:30 -0700

    10 mins update

---
+++ Lab-9/lab9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <vector>
+#include <string>
+using namespace std;
+
+const int ARRAY_SIZE = 30;
+
+int main(){
+    ifstream inFile("fighters.txt");
+    array<string, ARRAY_SIZE> fighterArray;
+}
