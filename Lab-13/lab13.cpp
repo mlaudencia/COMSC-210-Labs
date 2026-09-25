1118df2041a5d9f4197c19944a721f8e446c40fd
mlaudencia.cb@gmail.com <mlaudencia.cb@gmail.com>
2026-09-24 23:19:54 -0700

    10+ min update, forgot since commenting

--- Lab-13/lab13.cpp
+++ Lab-13/lab13.cpp
@@ -1,8 +1,16 @@
+// COMSC-210 | Lab 13 | Jeremy Laudencia
+
+// Here, I included <iostream> to handle the cout statements,
+// <fstream> to read and export data files,
+// and <cmath> to use square root for standard deviation.
 #include <iostream>
 #include <fstream>
 #include <cmath>
 using namespace std;
 
+// I made the NUM_STUDENTS a constant variable equal to 150,
+// since that's how many lines were in the data file, and since
+// I'm using a raw array and not a vector.
 const int NUM_STUDENTS = 150;
 
 struct Student{
@@ -10,6 +18,9 @@ struct Student{
     double grade;
 };
 
+// Here, I make prototypes for each thing in the program requirements.
+// I use Student* students so that I can access the actual values of
+// students and not a copy of them.
 void SelectionSort(Student* students, int NUM_STUDENTS);
 void minScore(Student* students, int NUM_STUDENTS);
 void maxScore(Student* students, int NUM_STUDENTS);
@@ -19,6 +30,8 @@ void sdScore(Student* students, int NUM_STUDENTS);
 
 int main(){
 
+    // Here, I make an array of size NUM_STUDENTS of type Student,
+    // then add the contents of the data file into the array by line.
     Student students[NUM_STUDENTS];
     ifstream inFile("210-lab-13-grades.txt");
 
@@ -35,8 +48,14 @@ int main(){
      cout << "Data read successfully!" << endl;
 
      cout << "Read " << NUM_STUDENTS << " student records" << endl;
+
+     // After reading the contents of the data file, we sort them by
+     // ascending ID, and then export it as another data file.
+     SelectionSort(students, NUM_STUDENTS);
      outputSortedStudents(students, NUM_STUDENTS, "210-lab-13-grades-sorted.txt");
 
+     // The below just prints out the statistics of the summary,
+     // which includes all the stuff in the program requirements.
      cout << " --- Summary Statistics ---" << endl;
      minScore(students, NUM_STUDENTS);
      maxScore(students, NUM_STUDENTS);
@@ -45,6 +64,11 @@ int main(){
      sdScore(students, NUM_STUDENTS);
 }
 
+
+// SelectionSort() takes in an array and its size, and sorts it by
+// ascending ID's.
+// arguments: Student* students, int NUM_STUDENTS
+// returns: nothing
 void SelectionSort(Student* students, int NUM_STUDENTS){
     for(int i = 0; i < NUM_STUDENTS - 1; i++){
         int smallIndex = i;
@@ -61,6 +85,10 @@ void SelectionSort(Student* students, int NUM_STUDENTS){
     }
 };
 
+// maxScore() takes in an array and its size, and prints what student
+// had the smallest score and their ID. 
+// arguments: Student* students, int NUM_STUDENTS
+// returns: nothing
 void minScore(Student* students, int NUM_STUDENTS){
     int smallScore = 0;
         for(int i = 1; i < NUM_STUDENTS; i++){
@@ -72,6 +100,10 @@ void minScore(Student* students, int NUM_STUDENTS){
     << " (Student ID: " << students[smallScore].id << ") " << endl;
 };
 
+// maxScore() takes in an array and its size, and prints what student
+// had the largest score and their ID. 
+// arguments: Student* students, int NUM_STUDENTS
+// returns: nothing
 void maxScore(Student* students, int NUM_STUDENTS){
     int highScore = 0;
         for(int i = 1; i < NUM_STUDENTS; i++){
@@ -83,6 +115,10 @@ void maxScore(Student* students, int NUM_STUDENTS){
     << " (Student ID: " << students[highScore].id << ") " << endl;
 };
 
+// findMeanScore() takes in an array and its size, and finds the
+// mean of the array, adding all its values and dividing it by size.
+// arguments: Student* students, int NUM_STUDENTS
+// returns: nothing
 double findMeanScore(Student* students, int NUM_STUDENTS){
     double total = 0;
     for(int i = 0; i < NUM_STUDENTS; i++){
@@ -91,11 +127,19 @@ double findMeanScore(Student* students, int NUM_STUDENTS){
     return total / NUM_STUDENTS;
 };
 
+// meanScore() takes in an array and its size, and prints what the
+// mean of all student scores is.
+// arguments: Student* students, int NUM_STUDENTS
+// returns: nothing
 void meanScore(Student* students, int NUM_STUDENTS){
     double meanScore = findMeanScore(students, NUM_STUDENTS);
     cout << "Mean score: " << meanScore << endl;
 }
 
+// medianScore() takes in an array and its size, and prints the ID's 
+// of the student or students who were in the middle of the array.
+// arguments: Student* students, int NUM_STUDENTS
+// returns: nothing
 void medianScore(Student* students, int NUM_STUDENTS){
     double median = 0;
     if(NUM_STUDENTS % 2 == 1){
